9cf2849454784c7bff48d98ea18e811cd4639913
Adam Laki <info@adamlaki.com>
2018-08-26 15:55:29 +0200

    Rewrite the codebase
    - Rewrite JS to a modern format
    - Add webpack support
    - Restructure Sass files
    - Modify Gulp builds

--- dist/canvi-webpack.js
+++ dist/canvi-webpack.js
@@ -266,3 +266,4 @@ class Canvi {
         console.groupEnd();
     }
 }
+export default Canvi;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,50 @@
-var gulp = require('gulp');  
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var prefix = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var gutil = require('gulp-util');
-var babel = require('gulp-babel');
+var gulp = require('gulp'), 
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	prefix = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	gutil = require('gulp-util'),
+	babel = require('gulp-babel'),
+	rename = require('gulp-rename'),
+	gap = require('gulp-append-prepend'),
+	strip = require('gulp-strip-comments'),
+	wait = require('gulp-wait');
 
 gulp.task('sass', function () {  
-    gulp.src('./scss/canvi.scss')
-        .pipe(sass({includePaths: ['scss']}))
-        .pipe(prefix(
+	gulp.src('./scss/canvi.scss')
+        .pipe(wait(700))
+		.pipe(sass({includePaths: ['./scss/partials']}))
+		.pipe(prefix(
 			'last 1 version', '> 1%', 'ie 8', 'ie 7'
 		))
-        .pipe(minifycss())
-        .pipe(gulp.dest('dist'));
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('uglify', function(){
+gulp.task('js', function(){
 	gulp.src('./js/*.js')
-		.pipe(babel())
+		.pipe(strip())
+		.pipe(babel({
+			presets: ['es2015'],
+			plugins: ['transform-object-assign']
+		}))
+		.pipe(uglify())
 		.on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['sass', 'uglify'], function () {  
-    gulp.watch('scss/*.scss', ['sass']);
-    gulp.watch('./js/*.js', ['uglify']);
+gulp.task('js-webpack', function(){
+	gulp.src('./js/*.js')
+		.pipe(strip())
+		.pipe(rename({
+			suffix: '-webpack'
+		}))
+		.pipe(gap.appendText('export default Canvi;'))
+		.on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['sass', 'js', 'js-webpack'], function () {  
+	gulp.watch('scss/**/*.scss', ['sass']);
+	gulp.watch('./js/*.js', ['js']);
 });
